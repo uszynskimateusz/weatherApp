ea7adea8369dee45a3c727f98560e407e6fbc231
Mateusz Uszyński <m.uszynski@futuremind.com>
2024-09-16 22:22:14 +0200

    App constants added and remove unused view controller

---
+++ Weather/Foundation/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//
+
+import Foundation
+
+enum Constants {
+    static let baseUrl = URL(string: "http://dataservice.accuweather.com")!
+    static let apiKey: String = <#API KEY#>
+}
