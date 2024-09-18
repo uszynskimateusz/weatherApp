ec4c251ce3a2480e55fa4cbfb1b5de90311c1acb
Mateusz Uszyński <m.uszynski@futuremind.com>
2024-09-18 19:47:29 +0200

    New images added

--- Weather/Foundation/Images.swift
+++ Weather/Foundation/Images.swift
@@ -8,4 +8,8 @@ enum Images {
     static let searchIcon: UIImage? = UIImage(systemName: "magnifyingglass")
     static let clearIcon: UIImage? = UIImage(systemName: "x.circle")
     static let chevronIcon: UIImage? = UIImage(systemName: "chevron.left")
+
+    static let rainIcon: UIImage? = UIImage(systemName: "cloud.rain")
+    static let snowIcon: UIImage? = UIImage(systemName: "snowflake")
+    static let windIcon: UIImage? = UIImage(systemName: "wind")
 }
