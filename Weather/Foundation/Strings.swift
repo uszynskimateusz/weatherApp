ee23394d98f0b9c4aff941b73e5f1228ac065986
Mateusz Uszyński <m.uszynski@futuremind.com>
2024-09-18 17:10:39 +0200

    Improve UI

--- Weather/Foundation/Strings.swift
+++ Weather/Foundation/Strings.swift
@@ -4,5 +4,6 @@
 
 enum Strings {
     static let welcomeTitle: String = "Wyszukaj miasto"
+    static let minimumTemperature: String = "Najniższa tempertura: "
     static let nextDays: String = "Nadchodzące dni"
 }
