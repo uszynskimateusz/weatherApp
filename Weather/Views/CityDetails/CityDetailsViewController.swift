7fae0356b6eba7418da29b55458718e625508172
Mateusz Uszyński <m.uszynski@futuremind.com>
2024-09-18 15:49:57 +0200

    City details view controller added

---
+++ Weather/Views/CityDetails/CityDetailsViewController.swift
@@ -0,0 +1,62 @@
+//
+//  CityDetailsViewController.swift
+//
+
+import UIKit
+
+final class CityDetailsViewController: UIViewController {
+    init(viewModel: CityDetailsViewModelProtocol) {
+        self.viewModel = viewModel
+
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @objc func backButtonTapped() {
+        self.navigationController?.popViewController(animated: true)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func loadView() {
+        view = layout
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setup()
+    }
+
+    private let layout = CityDetailsViewLayout()
+    private var viewModel: CityDetailsViewModelProtocol
+
+    private func setup() {
+        setupNavigationBar()
+        bindViewModel()
+    }
+
+    private func setupNavigationBar() {
+        title = viewModel.title
+        let backButton = UIBarButtonItem(
+            image: UIImage(systemName: "chevron.left"),
+            style: .plain,
+            target: self,
+            action: #selector(backButtonTapped)
+        )
+        backButton.tintColor = .black
+        navigationItem.leftBarButtonItem = backButton
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    private func bindViewModel() {
+        viewModel.onUpdateForecast = { [weak self] forecast in
+            guard let self else { return }
+
+            DispatchQueue.main.async {
+                self.layout.configure(forecast)
+            }
+        }
+    }
+}
