b94fdd570606aff52ad21ac5fd3f92857bcea3d6
Mateusz Uszyński <m.uszynski@futuremind.com>
2024-09-18 15:42:35 +0200

    Daily forecast endpoint added

---
+++ Weather/Networking/Endpoints/DailyForecastEndpoint.swift
@@ -0,0 +1,24 @@
+//
+//  DailyForecastEndpoint.swift
+//
+
+struct DailyForecastEndpoint: HttpEndpointProtocol {
+    let cityKey: String
+
+    init(cityKey: String) {
+        self.cityKey = cityKey
+    }
+
+    var path: String {
+        "forecasts/v1/daily/5day/\(cityKey)"
+    }
+    var method: HttpMethod = .get
+
+    var queryParams: [String : Any] {
+        [
+            "apikey": AppConstants.apiKey,
+            "metric": true,
+            "language": "pl"
+        ]
+    }
+}
