5e2251b1e0772b4789fd7b57b70f1ddea23bda9b
Mateusz Uszyński <m.uszynski@futuremind.com>
2024-09-18 20:57:30 +0200

    Overide user interface style

--- Weather/SceneDelegate.swift
+++ Weather/SceneDelegate.swift
@@ -19,6 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let window = UIWindow(windowScene: windowScene)
+        window.overrideUserInterfaceStyle = .light
         self.window = window
 
         let appCoordinator = AppCoordinator(window: window)
