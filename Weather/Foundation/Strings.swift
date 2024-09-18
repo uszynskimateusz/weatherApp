ed6c59b6491ff547a1aee25b4d1c4897519c03f0
Mateusz Uszyński <m.uszynski@futuremind.com>
2024-09-18 18:58:40 +0200

    New strings added

--- Weather/Foundation/Strings.swift
+++ Weather/Foundation/Strings.swift
@@ -6,4 +6,6 @@ enum Strings {
     static let welcomeTitle: String = "Wyszukaj miasto"
     static let minimumTemperature: String = "Najniższa tempertura: "
     static let nextDays: String = "Nadchodzące dni"
+    static let historySearchTitle: String = "Historia"
+    static let searchTitle: String = "Wyniki wyszukań"
 }
