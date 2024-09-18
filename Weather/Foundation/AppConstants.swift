5dda57c08e711cdae28eb29d3fccca3a99d4e407
Mateusz Uszyński <m.uszynski@futuremind.com>
2024-09-18 14:50:50 +0200

    Change city regex

--- Weather/Foundation/AppConstants.swift
+++ Weather/Foundation/AppConstants.swift
@@ -5,7 +5,7 @@
 import Foundation
 
 enum AppConstants {
-    static let cityRegex = "^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]+$"
+    static let cityRegex = "^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ ]*$"
     static let baseUrl = URL(string: "http://dataservice.accuweather.com")!
     static let apiKey: String = <#API KEY#>
 }
