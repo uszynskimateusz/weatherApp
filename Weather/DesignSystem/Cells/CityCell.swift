0e798316e303e35a9ba651725ed0b0532473ae75
Mateusz Uszyński <m.uszynski@futuremind.com>
2024-09-18 08:28:11 +0200

    City collection cell added

---
+++ Weather/DesignSystem/Cells/CityCell.swift
@@ -0,0 +1,43 @@
+//
+//  CityCell.swift
+//
+
+import UIKit
+
+class CityCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setup()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private let cityLabel = UILabel()
+
+    func configure(_ city: String) {
+        cityLabel.text = city
+    }
+
+    private func setup() {
+        setupLabel()
+    }
+
+    private func setupLabel() {
+        addSubview(cityLabel)
+
+        cityLabel.textColor = .black
+        cityLabel.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+
+        cityLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            cityLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            cityLabel.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            cityLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            cityLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+}
